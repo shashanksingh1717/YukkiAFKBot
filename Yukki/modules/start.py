b50030f6089ffb45af0c4eba993f4777c4169cb6
shashanksingh1717 <107427465+shashanksingh1717@users.noreply.github.com>
2022-09-03 11:55:25 +0530

    Update start.py

--- Yukki/modules/start.py
+++ Yukki/modules/start.py
@@ -78,7 +78,7 @@ async def on_private_start(_, message: Message):
             ]
         )
         image = random.choice(RANDOM)
-        await message.reply_photo(image, caption=f"Hello! My name is {botname}.\n\nTo know more about me check help section by /help. Active since {Uptime}", reply_markup=upl)
+        await message.reply_photo(image, caption=f"ʜᴇʟʟᴏ! ᴍʏ ɴᴀᴍᴇ ɪꜱ {botname}.\n\n ᴋɴᴏᴡ ᴍᴏʀᴇ ᴀʙᴏᴜᴛ ᴍᴇ ᴄʜᴇᴄᴋ /help ꜱᴇᴄᴛɪᴏɴ. ᴀᴄᴛɪᴠᴇ ꜱɪɴᴄᴇ {Uptime}", reply_markup=upl)
 
 @app.on_message(filters.command(["help"]) & filters.private & ~filters.edited)
 async def on_private_help(_, message: Message):
